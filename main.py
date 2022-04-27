6f397ff64e507566a6d4b3a58f4c11e4baaf10af
shibadisaster <anpetoron@gmail.com>
2022-04-27 16:52:10 +0800

    Added image colorizing from polynomials

--- main.py
+++ main.py
@@ -41,8 +41,9 @@ b_values = []
 
 for i in range(num_points):
     point_x = (2 * i + 1) * (width / (num_points * 2))
-    point_x = round(point_x)
-    x_positions.append((point_x / width) * scale)
+    point_x = int(point_x)
+    point_x_scaled = (2 * i + 1) * (scale / (num_points * 2))
+    x_positions.append(point_x_scaled)
     
     rgb_value = inp.getpixel( (point_x, mid_height) )
     r_values.append(rgb_value[0])
@@ -104,24 +105,62 @@ new_image.show()
 
 
 
-#c/o harvey, so far only accurate when scale == width
-if scale == width:
-    print()
+#c/o harvey
+print()
+
+r_error = 0
+g_error = 0
+b_error = 0
+
+for i in range(width):
+    point_x_scaled = (2 * i + 1) * (scale / (width * 2))
     
-    error = 0
-    for i in range(width):
-        x_positions.append(i)
-        
-        rgb_value = inp.getpixel( (i, mid_height) )
-        
-        r = min(max(np.poly1d(r_coefficients)(i), 0), 255)
-        g = min(max(np.poly1d(g_coefficients)(i), 0), 255)
-        b = min(max(np.poly1d(b_coefficients)(i), 0), 255)
-        
-        error += abs(rgb_value[0] - r)
-        error += abs(rgb_value[1] - g)
-        error += abs(rgb_value[2] - b)
-        
-    print(f"Average error (out of 255): {error/width/3}")
+    rgb_value = inp.getpixel( (i, mid_height) )
+    
+    r = min(max(np.poly1d(r_coefficients)(point_x_scaled), 0), 255)
+    g = min(max(np.poly1d(g_coefficients)(point_x_scaled), 0), 255)
+    b = min(max(np.poly1d(b_coefficients)(point_x_scaled), 0), 255)
+    
+    r_error += abs(rgb_value[0] - r)
+    g_error += abs(rgb_value[1] - g)
+    b_error += abs(rgb_value[2] - b)
+    
+print(f"Average error R channel: {r_error / width} / 255")
+print(f"Average error G channel: {g_error / width} / 255")
+print(f"Average error B channel: {b_error / width} / 255")
 
 
+
+
+
+
+#grayscale colorizing
+grayscale = np.asarray(Image.open("grayscale2.png"))
+grayscale = grayscale.tolist()
+
+compressed_grayscale = []
+for row in grayscale:
+    compressed_row = []
+    for pixel in row:
+        #okay for some reason the grayscale images are read differently, grayscale.png is read as rgb while grayscale2.png gets read as a grayscale image
+        #for grayscale.png this line should be compressed_row.append(pixel[0] * scale / 255) || this just reads red since it should be grayscale anyway
+        #for grayscale2.png this line should be compressed_row.append(pixel * scale / 255)
+        compressed_row.append(pixel * scale / 255)
+    compressed_grayscale.append(compressed_row)
+
+colorized = []
+for row in compressed_grayscale:
+    colorized_row = []
+    for pixel in row:
+        colorized_pixel = [0 for _ in range(3)]
+        colorized_pixel[0] = min(max(np.poly1d(r_coefficients)(pixel), 0), 255)
+        colorized_pixel[1] = min(max(np.poly1d(g_coefficients)(pixel), 0), 255)
+        colorized_pixel[2] = min(max(np.poly1d(b_coefficients)(pixel), 0), 255)
+        
+        colorized_row.append(colorized_pixel)
+        
+    colorized.append(colorized_row)
+
+colorized = np.array(colorized, dtype=np.uint8)
+colorized = Image.fromarray(colorized)
+colorized.show()
